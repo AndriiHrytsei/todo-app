255f04017c3336377f386cbb8bbdf59b8453d8e3
AndriiHrytsei <AndriiHrytsei@users.noreply.github.com>
2024-07-26 14:04:07 +0000

    deploy: e3c25cd2ac7f525629a4ef3d1b31ac3fb7217378

---
+++ assets/Loading-BykGNb3K.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CGcBWU96.js";const t=()=>o.jsx("h1",{children:"Loading"});export{t as default};
--- assets/Loading-DdqeCE3w.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-D0fo8mdB.js";const t=()=>o.jsx("h1",{children:"Loading"});export{t as default};
