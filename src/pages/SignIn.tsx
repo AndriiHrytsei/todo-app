a5ebb2af6213df0ebffc47b68b159d658eea3d7d
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2024-07-26 17:02:53 +0300

    add: restricted paths

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { useEffect } from "react";
 import SignInForm from "../components/SignInForm/SignInForm";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config/firebase";
+import { Link, useNavigate } from "react-router-dom";
+
 
 const SignIn = () => {
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate("/tasks")
+      }
+    })
+  }, [])
+
   return (
     <main>
+      <Link to="/signUp">Sign up</Link>
       <h1>Sign in</h1>
       <SignInForm />
     </main>
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,40 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { logOut } from '../redux/operations'
-import { useAppDispatch } from '../redux/store'
-import { useNavigate } from 'react-router-dom'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from '../config/firebase'
+import React, { useEffect, useState } from "react";
+import { logOut } from "../redux/operations";
+import { useAppDispatch } from "../redux/store";
+import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config/firebase";
 
 const Tasks = () => {
-
-  const [heading, setHeading] = useState("")
-
-  const dispatch = useAppDispatch()
-  const navigate = useNavigate()
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const handleButtonClick = () => {
-    dispatch(logOut())
-    navigate('/signIn')
-  }
+    dispatch(logOut());
+    navigate("/");
+  };
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setHeading("You are signed in!")
-        console.log(auth?.currentUser);
-      } else {
-        setHeading("You are not signed in.")
-        console.log(auth?.currentUser);
+      if (!user) {
+        navigate("/");
       }
     });
   }, []);
 
   return (
     <main>
-      <h1>{heading}</h1>
-      <button type="button" onClick={handleButtonClick}>logout</button>
+      <h1>Tasks</h1>
+      <button type="button" onClick={handleButtonClick}>
+        logout
+      </button>
     </main>
-  )
-}
+  );
+};
 
-export default Tasks
+export default Tasks;
