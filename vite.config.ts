95d785ea39dfaaceed4b530cf8d4065669b4796c
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2024-09-29 18:04:41 +0200

    fix hosting

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //! When creating a project using this template, 
-  //! change the value of "base" to the name of your repository
-  base: "/todo-app/",
 })
